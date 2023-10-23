d296cb5373fee4525fc1c11db4c51c980cf57114
Edison <solisedison@outlook.com>
2023-10-22 19:14:33 -0500

    Start aplication users

--- Save_money/settings.py
+++ Save_money/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'proyects',
-    'rest_framework'
+    'rest_framework',
+    'knox'
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,7 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+}
--- proyects/urls.py
+++ proyects/urls.py
@@ -1,5 +1,6 @@
-from rest_framework import routers
-from .api import ProjectViewSet
-router=routers.DefaultRouter()
-router.register('api/proyects',ProjectViewSet,'proyects')
-urlpatterns=router.urls
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('/login/',views.login_api)
+]
