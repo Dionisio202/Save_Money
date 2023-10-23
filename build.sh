e129cb1aab54ba0c4040054a7e9d2f4fe2e2afce
Edison <solisedison@outlook.com>
2023-10-23 09:10:00 -0500

    superusercreator

--- build.sh
+++ build.sh
@@ -4,5 +4,11 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Ejecuta el comando para aplicar las migraciones
 python manage.py migrate
+
+# Crea un superusuario (sustituye 'superuser' por los valores que desees)
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('dionisio', 'solisedison@outlook.com', 'EdisonsolIs202.')" | python manage.py shell
+
+# Recoge archivos estáticos
+python manage.py collectstatic --no-input
