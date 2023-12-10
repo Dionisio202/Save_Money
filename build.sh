3fbae8a5904e9f3eccc459101a257bf84699207d
Edison <solisedison@outlook.com>
2023-12-09 20:53:23 -0500

    fixed

--- build.sh
+++ build.sh
@@ -8,8 +8,7 @@ pip install -r requirements.txt
 python manage.py migrate
 
 # Crea un superusuario (sustituye 'superuser' por los valores que desees)
-#echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('dionisio202', 'solisedison@outlook.com', 'EdisonsolIs202.')" | python manage.py shell
-pip install six
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('dionisio202', 'solisedison@outlook.com', 'EdisonsolIs202.')" | python manage.py shell
 
 # Recoge archivos estáticos
 python manage.py collectstatic --no-input
