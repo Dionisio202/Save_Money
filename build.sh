7b11364e86ff38063a8faf2f40a1060af95423ec
Edison <solisedison@outlook.com>
2023-12-09 20:47:05 -0500

    added dependency six

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ python manage.py migrate
 
 # Crea un superusuario (sustituye 'superuser' por los valores que desees)
 #echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('dionisio202', 'solisedison@outlook.com', 'EdisonsolIs202.')" | python manage.py shell
+pip install six
 
 # Recoge archivos estáticos
 python manage.py collectstatic --no-input
